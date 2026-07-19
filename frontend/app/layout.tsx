73accdbcfeda9118cf1ec827aee6b3fe3d176c29
Sujithkrys <thalathotysujith@gmail.com>
2026-07-19 21:58:55 +0530

    Use tailwindcss cli to ensure styling works on Cloudflare

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./generated.css";
 import Sidebar from "@/components/Sidebar";
 import ThemeProvider from "@/components/ThemeProvider";
 
